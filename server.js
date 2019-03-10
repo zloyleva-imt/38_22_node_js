ea464262879d090aec38c81330e8a28093db8adf
ZloyLeva <zloyleva@gmail.com>
2019-03-10 19:27:25 +0200

    add CORS

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const jsonParser = bodyParser.json();
 
 app.use((req,res, next) => {
     res.header("Access-Control-Allow-Origin","*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
     return next();
