141c3eba6ada9402d24474f579802d8a754a3373
ZloyLeva <zloyleva@gmail.com>
2019-03-10 16:39:11 +0200

    create Cart model

---
+++ migrations/20190310143739-create-cart.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      amount: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Carts');
+  }
+};
